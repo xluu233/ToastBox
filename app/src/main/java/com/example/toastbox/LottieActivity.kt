652d6d34f9491f4b5417b1641579dc4e22d67e7e
xlu <luhongchen@foxmail.com>
2021-08-12 20:25:26 +0800

    完善演示Demo

--- app/src/main/java/com/example/toastbox/LottieActivity.kt
+++ app/src/main/java/com/example/toastbox/LottieActivity.kt
@@ -11,26 +11,34 @@ class LottieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lottie)
+
+        showAll()
+    }
+
+    private fun showAll() {
+        ToastBox().setView(R.layout.toast_lottie_success).setXY(0,100).show("Success!")
+        ToastBox().setView(R.layout.toast_lottie_error).setXY(0,500).show("Net Error!")
+        ToastBox().setView(R.layout.toast_lottie_fail).setXY(0,900).show("Failed!")
     }
 
     fun toast(view: View) {
         when(view.id){
             R.id.button13 -> {
                 //info
-                ToastBox().setView(R.layout.toast_lottie_error).show("Error")
+                showAll()
+                //ToastBox().setView(R.layout.toast_lottie_info).show("Show Message~")
             }
             R.id.button14 -> {
                 //net error
-                ToastBox().setView(R.layout.toast_lottie_error).show("Error")
+                ToastBox().setView(R.layout.toast_lottie_error).show("Net Error!")
             }
             R.id.button15 -> {
                 //success
-
+                ToastBox().setView(R.layout.toast_lottie_success).show("Success!")
             }
             R.id.button16 -> {
                 //fail
-
-
+                ToastBox().setView(R.layout.toast_lottie_fail).show("Failed!")
             }
         }
     }
